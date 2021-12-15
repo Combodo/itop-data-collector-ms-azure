f0ce5c943cb5aa7438c74f024d4c69bb79f6bc18
xtophe38 <christophe.naud@gmail.com>
2021-12-15 18:37:18 +0100

    Improve management of object dependencies

--- AzureResourceGroupJsonCollector.class.inc.php
+++ AzureResourceGroupJsonCollector.class.inc.php
@@ -8,12 +8,29 @@ class AzureResourceGroupJsonCollector extends AzureJsonCollector {
 		return $this->sResource.'/subscriptions/'.$iSubscription.'/resourcegroups?api-version='.$this->sApiVersion;
 	}
 
+	protected function BuildUrl($aParameters): string {
+		if (!array_key_exists(self::URI_PARAM_SUBSCRIPTION, $aParameters)) {
+			return '';
+		} else {
+			return $this->sResource.'subscriptions/'.$aParameters[self::URI_PARAM_SUBSCRIPTION].'/resourcegroups?api-version='.$this->sApiVersion;
+		}
+	}
+
 	/**
 	 * @inheritdoc
 	 */
 	protected function ReportResourceGroups($aData, $iSubscription): void {
 		foreach ($aData['value'] as $aResource) {
 			$this->oAzureCollectionPlan->AddResourceGroupsToConsider($iSubscription, $aResource['name']);
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ReportObjects($aData, $sObjectL1, $sObjectL2): void {
+		foreach ($aData['value'] as $aObject) {
+			$this->oAzureCollectionPlan->AddAzureObjectsToConsider($sObjectL1, $aObject['name'], null);
 		}
 	}
 
