dc138e59516d2222f64b9eb23b29cb1690926d26
xtophe38 <christophe.naud@gmail.com>
2021-12-15 18:35:21 +0100

    Add classes to collect

--- main.php
+++ main.php
@@ -4,9 +4,16 @@ require_once(APPROOT.'collectors/AzureJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureAppServiceJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureCloudServiceJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureContainerRegistryJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureCosmosDBAccountJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureDiskJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureFabricClusterJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureIoTHubJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureLoadBalancerJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureLocationJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureMariaDBServerJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureMariaDBJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureResourceGroupJsonCollector.class.inc.php');
+require_once(APPROOT.'collectors/AzureStorageAccountJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureSubscriptionJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureVirtualMachineJsonCollector.class.inc.php');
 require_once(APPROOT.'collectors/AzureVNetJsonCollector.class.inc.php');
@@ -20,8 +27,15 @@ $aCollectorClasses = [
 	'AzureResourceGroupJsonCollector',      // 3- Resources group
 	'AzureAppServiceJsonCollector',         // App Services
 	'AzureCloudServiceJsonCollector',       // Cloud Services
+	'AzureCosmosDBAccountJsonCollector',    // Cosmos DB Accounts
 	'AzureContainerRegistryJsonCollector',  // Container Registries
+	'AzureDiskJsonCollector',               // Disks
+	'AzureFabricClusterJsonCollector',      // Fabric Clusters
+	'AzureIoTHubJsonCollector',             // IoT Hub
 	'AzureLoadBalancerJsonCollector',       // Load Balancers
+	'AzureMariaDBServerJsonCollector',      // Maria DB Servers
+	'AzureMariaDBJsonCollector',            // Maria DBs
+	'AzureStorageAccountJsonCollector',     // Storage Accounts
 	'AzureVirtualMachineJsonCollector',     // Virtual Machines
 	'AzureVNetJsonCollector',               // Virtual Networks
 	'AzureVNetGatewayJsonCollector',        // Virtual Network gateways
