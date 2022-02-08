5565060cf78fc65662b5651e68aa9c1ab52066be
xtophe38 <christophe.naud@gmail.com>
2022-02-08 18:50:31 +0100

    Improvement for standard Azure collector V1

---
+++ AzureDiskAzureCollector.class.inc.php
@@ -0,0 +1,119 @@
+<?php
+require_once(APPROOT.'collectors/MSJsonCollector.class.inc.php');
+
+class AzureDiskAzureCollector extends MSJsonCollector
+{
+	// Required parameters to build URL
+	protected static $aURIParameters = [
+		1 => self::URI_PARAM_SUBSCRIPTION,
+		2 => self::URI_PARAM_RESOURCEGROUP,
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function BuildUrl($aParameters): string
+	{
+		if (!array_key_exists(self::URI_PARAM_SUBSCRIPTION, $aParameters) || !array_key_exists(self::URI_PARAM_RESOURCEGROUP,
+				$aParameters)) {
+			return '';
+		} else {
+			$sUrl = $this->sResource.'subscriptions/'.$aParameters[self::URI_PARAM_SUBSCRIPTION];
+			$sUrl .= '/resourceGroups/'.$aParameters[self::URI_PARAM_RESOURCEGROUP];
+			$sUrl .= '/providers/Microsoft.Compute/disks?api-version='.$this->sApiVersion;
+
+			return $sUrl;
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function MustProcessBeforeSynchro(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Prepare(): bool
+	{
+		// Create MappingTable
+		$this->oStatusMapping = new MappingTable('azureci_provisioning_state_mapping');
+
+		return parent::Prepare();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function DoLookup($aLookupKey, $sDestField): array
+	{
+		$sResult = false;
+		$sData = '';
+		switch ($sDestField) {
+			case 'azureresourcegroup_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'resourceGroups');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			case 'azuresubscription_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'subscriptions');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		return [$sResult, $sData];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+	{
+		// Process each line of the CSV
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azureresourcegroup_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process specific data
+			$aData['provisioning_status'] = $this->oStatusMapping->MapValue($aData['provisioning_status'], 'succeeded');
+			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
+			$aData['azurestatus'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['diskState'];
+			$aData['osfamily_id'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['osType'];
+			$aData['size'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['diskSizeGB'];
+			$aData['sku'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['sku']['name'];
+		}
+
+		return $aData;
+	}
+
+}
+
---
+++ AzureResourceGroupAzureCollector.class.inc.php
@@ -0,0 +1,108 @@
+<?php
+require_once(APPROOT.'collectors/AzureCollectionPlan.class.inc.php');
+require_once(APPROOT.'collectors/MSJsonCollector.class.inc.php');
+
+class AzureResourceGroupAzureCollector extends MSJsonCollector
+{
+	// Required parameters to build URL
+	protected static $aURIParameters = [
+		1 => self::URI_PARAM_SUBSCRIPTION,
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function BuildUrl($aParameters): string
+	{
+		if (!array_key_exists(self::URI_PARAM_SUBSCRIPTION, $aParameters)) {
+			return '';
+		} else {
+			$sUrl = $this->sResource.'subscriptions/'.$aParameters[self::URI_PARAM_SUBSCRIPTION];
+			$sUrl .= '/resourcegroups?api-version='.$this->sApiVersion;
+
+			return $sUrl;
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ReportObjects($aData, $sObjectL1, $sObjectL2): void
+	{
+		foreach ($aData['value'] as $aObject) {
+			$this->oMSCollectionPlan->AddMSObjectsToConsider($sObjectL1, $aObject['name'], null);
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function MustProcessBeforeSynchro(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Prepare(): bool
+	{
+		// Create MappingTable
+		$this->oStatusMapping = new MappingTable('azureci_provisioning_state_mapping');
+
+		return parent::Prepare();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function DoLookup($aLookupKey, $sDestField): array
+	{
+		$sResult = false;
+		$sData = '';
+		switch ($sDestField) {
+			case 'azuresubscription_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'subscriptions');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		return [$sResult, $sData];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+	{
+		// Process each line of the CSV
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process each collected status
+			$aData['provisioning_status'] = $this->oStatusMapping->MapValue($aData['provisioning_status'], 'succeeded');
+		}
+
+		return $aData;
+	}
+
+}
+
