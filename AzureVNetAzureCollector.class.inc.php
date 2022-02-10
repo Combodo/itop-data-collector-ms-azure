d145aff30fae631cfe3d6682dce2e26ec98c3619
xtophe38 <christophe.naud@gmail.com>
2022-02-10 17:16:14 +0100

    Adjust processing of some attributes

--- AzureVNetAzureCollector.class.inc.php
+++ AzureVNetAzureCollector.class.inc.php
@@ -34,6 +34,17 @@ class AzureVNetAzureCollector extends MSJsonCollector
 	/**
 	 * @inheritdoc
 	 */
+	public function Prepare(): bool
+	{
+		// Create MappingTable
+		$this->oStatusMapping = new MappingTable('azureci_provisioning_state_mapping');
+
+		return parent::Prepare();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
 	protected function DoLookup($aLookupKey, $sDestField): array
 	{
 		$sResult = false;
@@ -80,6 +91,23 @@ class AzureVNetAzureCollector extends MSJsonCollector
 		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
 			throw new IgnoredRowException('Unknown code');
 		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process specific data
+			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
+			$aData['provisioning_status'] = $this->oStatusMapping->MapValue($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['provisioningState'],
+				'succeeded');
+			$aData['address_space'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['addressSpace']['addressPrefixes']['0'];
+		}
+
+		return $aData;
 	}
 
 }
