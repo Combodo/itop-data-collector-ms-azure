d21b6f07bc7ec4fe13b4cbb680596db6e8acb6a6
xtophe38 <christophe.naud@gmail.com>
2022-03-01 16:36:52 +0100

    Improvement for standard Azure collector V1

--- AzureAppServiceAzureCollector.class.inc.php
+++ AzureAppServiceAzureCollector.class.inc.php
@@ -39,6 +39,17 @@ class AzureAppServiceAzureCollector extends MSJsonCollector
 		$sResult = false;
 		$sData = '';
 		switch ($sDestField) {
+			case 'app_service_plan':
+				if (array_key_exists('app_service_plan', $aLookupKey) && ($aLookupKey['app_service_plan'] != '')) {
+					$sData = strstr($aLookupKey['app_service_plan'], 'serverfarms');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
 			case 'azureresourcegroup_id':
 				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
 					$sData = strstr($aLookupKey['primary_key'], 'resourceGroups');
@@ -74,12 +85,33 @@ class AzureAppServiceAzureCollector extends MSJsonCollector
 	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
 	{
 		// Process each line of the CSV
+		if (!$this->Lookup($aLineData, array('app_service_plan'), 'app_service_plan', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
 		if (!$this->Lookup($aLineData, array('primary_key'), 'azureresourcegroup_id', $iLineIndex, true, false)) {
 			throw new IgnoredRowException('Unknown code');
 		}
 		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
 			throw new IgnoredRowException('Unknown code');
 		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process specific data
+			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
+			$aData['app_service_plan'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['serverFarmId'];
+			$aData['ftps_hostname'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['ftpsHostName'];
+			$aData['status'] = strtolower($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['state']);
+			$aData['url'] = 'https://'.$this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['defaultHostName'];
+		}
+
+		return $aData;
 	}
 
 }
