d42c72d66487d8e5d69a5689ea930357ac361470
xtophe38 <christophe.naud@gmail.com>
2022-03-03 14:45:03 +0100

    File creation

---
+++ AzureKubernetesServiceAzureCollector.class.inc.php
@@ -0,0 +1,107 @@
+<?php
+require_once(APPROOT.'collectors/MSJsonCollector.class.inc.php');
+
+class AzureKubernetesServiceAzureCollector extends MSJsonCollector
+{
+	// Required parameters to build URL
+	protected static $aURIParameters = [
+		1 => self::URI_PARAM_SUBSCRIPTION,
+		2 => self::URI_PARAM_RESOURCEGROUP,
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function BuildUrl($aParameters): string
+	{
+		if (!array_key_exists(self::URI_PARAM_SUBSCRIPTION, $aParameters) || !array_key_exists(self::URI_PARAM_RESOURCEGROUP,
+				$aParameters)) {
+			return '';
+		} else {
+			$sUrl = $this->sResource.'subscriptions/'.$aParameters[self::URI_PARAM_SUBSCRIPTION];
+			$sUrl .= '/resourceGroups/'.$aParameters[self::URI_PARAM_RESOURCEGROUP];
+			$sUrl .= '/providers/Microsoft.ContainerService/managedClusters?api-version='.$this->sApiVersion;
+
+			return $sUrl;
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function MustProcessBeforeSynchro(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function DoLookup($aLookupKey, $sDestField): array
+	{
+		$sResult = false;
+		$sData = '';
+		switch ($sDestField) {
+			case 'azureresourcegroup_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'resourceGroups');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			case 'azuresubscription_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'subscriptions');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		return [$sResult, $sData];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+	{
+		// Process each line of the CSV
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azureresourcegroup_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process specific data
+			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
+			$aData['fqdn'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['fqdn'];
+			$aData['max_agent_pool'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['maxAgentPools'];
+			$aData['provisioning_status'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['provisioningState'];
+			$aData['version'] = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['kubernetesVersion'];
+		}
+
+		return $aData;
+	}
+
+}
+
