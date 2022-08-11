fb30fead6107ed9ea22edb31c87d0a63319ca0c2
xtophe38 <christophe.naud@gmail.com>
2022-08-11 18:57:24 +0200

    Create collecto files for AzureSubnet

---
+++ AzureSubnetAzureCollector.class.inc.php
@@ -0,0 +1,148 @@
+<?php
+require_once(APPROOT.'collectors/msbase/MSJsonCollector.class.inc.php');
+
+class AzureSubnetAzureCollector extends MSJsonCollector
+{
+	// Required parameters to build URL
+	protected static $aURIParameters = [
+		1 => self::URI_PARAM_SUBSCRIPTION,
+		2 => self::URI_PARAM_RESOURCEGROUP,
+		3 => self::URI_PARAM_VNET,
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	public function AttributeIsOptional($sAttCode): bool
+	{
+		if ($sAttCode == 'services_list') {
+			return true;
+		}
+
+		return parent::AttributeIsOptional($sAttCode);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function BuildUrl($aParameters): string
+	{
+		if (!array_key_exists(self::URI_PARAM_SUBSCRIPTION, $aParameters) || !array_key_exists(self::URI_PARAM_RESOURCEGROUP,
+				$aParameters) || !array_key_exists(self::URI_PARAM_VNET, $aParameters)) {
+			return '';
+		} else {
+			$sUrl = $this->sResource.'subscriptions/'.$aParameters[self::URI_PARAM_SUBSCRIPTION];
+			$sUrl .= '/resourceGroups/'.$aParameters[self::URI_PARAM_RESOURCEGROUP];
+			$sUrl .= '/providers/Microsoft.Network/virtualNetworks/'.$aParameters[self::URI_PARAM_VNET];
+			$sUrl .= '/subnets?api-version='.$this->sApiVersion;
+
+			return $sUrl;
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function MustProcessBeforeSynchro(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Prepare(): bool
+	{
+		// Create MappingTable
+		$this->oSubnetMaskMapping = new MappingTable('subnet_mask_mapping');
+
+		return parent::Prepare();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function DoLookup($aLookupKey, $sDestField): array
+	{
+		$sResult = false;
+		$sData = '';
+		switch ($sDestField) {
+			case 'azureresourcegroup_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'resourceGroups');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			case 'azuresubscription_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'subscriptions');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			case 'azurevnet_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'virtualNetworks');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		return [$sResult, $sData];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+	{
+		// Process each line of the CSV
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azureresourcegroup_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azurevnet_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process specific data
+			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
+			$aData['provisioning_status'] = strtolower($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['provisioningState']);
+			$aData['ip'] = strstr($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['addressPrefix'], '/', true);
+			$aData['mask'] = $this->oSubnetMaskMapping->MapValue(trim(strstr($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['addressPrefix'], '/'), '/'), '');
+			if (array_key_exists('delegations', $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties'])) {
+
+			}
+		}
+
+		return $aData;
+	}
+
+}
+
