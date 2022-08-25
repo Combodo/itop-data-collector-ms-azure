09a5911d1d269bca2d294f1c12a03cf893c24c89
xtophe38 <christophe.naud@gmail.com>
2022-08-25 09:54:44 +0200

    Add AzureLoadBalancerFrontendIPConfig collect

---
+++ AzureLoadBalancerFrontendIPConfigAzureCollector.class.inc.php
@@ -0,0 +1,138 @@
+<?php
+require_once(APPROOT.'collectors/msbase/MSJsonCollector.class.inc.php');
+
+class AzureLoadBalancerFrontendIPConfigAzureCollector extends MSJsonCollector
+{
+	// Required parameters to build URL
+	protected static $aURIParameters = [
+		1 => self::URI_PARAM_SUBSCRIPTION,
+		2 => self::URI_PARAM_RESOURCEGROUP,
+		3 => self::URI_PARAM_LOADBALANCER,
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	public function AttributeIsOptional($sAttCode): bool
+	{
+		if ($sAttCode == 'services_list') {
+			return true;
+		}
+
+		return parent::AttributeIsOptional($sAttCode);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function BuildUrl($aParameters): string
+	{
+		if (!array_key_exists(self::URI_PARAM_SUBSCRIPTION, $aParameters) || !array_key_exists(self::URI_PARAM_RESOURCEGROUP,
+				$aParameters) || !array_key_exists(self::URI_PARAM_LOADBALANCER, $aParameters)) {
+			return '';
+		} else {
+			$sUrl = $this->sResource.'subscriptions/'.$aParameters[self::URI_PARAM_SUBSCRIPTION];
+			$sUrl .= '/resourceGroups/'.$aParameters[self::URI_PARAM_RESOURCEGROUP];
+			$sUrl .= '/providers/Microsoft.Network/loadBalancers/'.$aParameters[self::URI_PARAM_LOADBALANCER];
+			$sUrl .= '/frontendIPConfigurations?api-version='.$this->sApiVersion;
+
+			return $sUrl;
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function MustProcessBeforeSynchro(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function DoLookup($aLookupKey, $sDestField): array
+	{
+		$sResult = false;
+		$sData = '';
+		switch ($sDestField) {
+			case 'azureresourcegroup_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'resourceGroups');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			case 'azuresubscription_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'subscriptions');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			case 'azureloadbalancer_id':
+				if (array_key_exists('primary_key', $aLookupKey) && ($aLookupKey['primary_key'] != '')) {
+					$sData = strstr($aLookupKey['primary_key'], 'loadBalancers');
+					if ($sData !== false) {
+						$aData = explode('/', $sData);
+						$sData = $aData[1];
+						$sResult = true;
+					}
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		return [$sResult, $sData];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+	{
+		// Process each line of the CSV
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azureresourcegroup_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azuresubscription_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+		if (!$this->Lookup($aLineData, array('primary_key'), 'azureloadbalancer_id', $iLineIndex, true, false)) {
+			throw new IgnoredRowException('Unknown code');
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function Fetch()
+	{
+		$aData = parent::Fetch();
+		if ($aData !== false) {
+			// Then process specific data
+			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
+			$sProperties = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties'];
+			$aData['provisioning_status'] = strtolower($sProperties['provisioningState']);
+			$aData['private_ip'] = $sProperties['privateIPAddress'];
+			$aData['private_ip_version'] = strtolower($sProperties['privateIPAddressVersion']);
+			$aData['private_ip_allocation_method'] = $sProperties['privateIPAllocationMethod'];
+			if (array_key_exists('subnet', $sProperties)) {
+				$aData['azuresubnet_id'] = $sProperties['subnet']['id'];
+			}
+		}
+
+		return $aData;
+	}
+
+}
