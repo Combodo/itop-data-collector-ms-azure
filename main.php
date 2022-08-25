afaa76a1f1013bfaf4e1b491f2135f4ca6b44bcf
xtophe38 <christophe.naud@gmail.com>
2022-08-25 10:14:15 +0200

    Add AzureLoadBalancerFrontendIPConfig collect

--- main.php
+++ main.php
@@ -3,29 +3,30 @@
 // List of collector to potentially launch.
 // --->>> Warning: order is important for some classes, the ones listed with numbers or letters; alphabetical order is ok for others <<<---
 $aCollectorClasses = [
-	'AzureSubscriptionAzureCollector',              // 1- Subscriptions
-	'AzureLocationAzureCollector',                  // 2- Locations
-	'AzureResourceGroupAzureCollector',             // 3- Resources group
-	'AzureAppServiceAzureCollector',                // App Services
-	'AzureKubernetesServiceAzureCollector',         // Kubernetes Services
-	'AzureLoadBalancerAzureCollector',              // Load Balancers
-	'AzureMariaDBServerAzureCollector',             // Maria DB Servers
-	'AzureMariaDBAzureCollector',                   // Maria DBs
-	'AzureMySQLServerAzureCollector',               // MySQL DB Servers
-	'AzureMySQLAzureCollector',                     // MySQL DBs
-	'AzureMSSQLServerAzureCollector',               // MS SQL DB Servers
-	'AzureMSSQLAzureCollector',                     // MS SQL DBs
-	'AzurePostgreServerAzureCollector',             // PostgreSQL Servers
-	'AzurePostgreAzureCollector',                   // PostgreSQL DBs
-	'AzureStorageAccountAzureCollector',            // Storage Accounts
-	'AzureVirtualMachineAzureCollector',            // a- Virtual Machines
-	'AzureDiskAzureCollector',                      // b- Disks
-	'AzureVNetAzureCollector',                      // c- Virtual Networks
-	'AzureNetworkInterfaceAzureCollector',          // d- Network Interfaces
-	'AzureSubnetAzureCollector',                    // e- Subnets
-	'AzurePublicIPAddressAzureCollector',           // f- Public IP Addresses
-	'AzureNetworkInterfaceIPConfigAzureCollector',  // g- Network Interface IP Configuration
-	'AzureVNetGatewayAzureCollector',               // Virtual Network gateways
+	'AzureSubscriptionAzureCollector',                  // 1- Subscriptions
+	'AzureLocationAzureCollector',                      // 2- Locations
+	'AzureResourceGroupAzureCollector',                 // 3- Resources group
+	'AzureAppServiceAzureCollector',                    // App Services
+	'AzureKubernetesServiceAzureCollector',             // Kubernetes Services
+	'AzureLoadBalancerAzureCollector',                  // Load Balancers
+	'AzureMariaDBServerAzureCollector',                 // Maria DB Servers
+	'AzureMariaDBAzureCollector',                       // Maria DBs
+	'AzureMySQLServerAzureCollector',                   // MySQL DB Servers
+	'AzureMySQLAzureCollector',                         // MySQL DBs
+	'AzureMSSQLServerAzureCollector',                   // MS SQL DB Servers
+	'AzureMSSQLAzureCollector',                         // MS SQL DBs
+	'AzurePostgreServerAzureCollector',                 // PostgreSQL Servers
+	'AzurePostgreAzureCollector',                       // PostgreSQL DBs
+	'AzureStorageAccountAzureCollector',                // Storage Accounts
+	'AzureVirtualMachineAzureCollector',                // a- Virtual Machines
+	'AzureDiskAzureCollector',                          // b- Disks
+	'AzureVNetAzureCollector',                          // c- Virtual Networks
+	'AzureNetworkInterfaceAzureCollector',              // d- Network Interfaces
+	'AzureSubnetAzureCollector',                        // e- Subnets
+	'AzurePublicIPAddressAzureCollector',               // f- Public IP Addresses
+	'AzureNetworkInterfaceIPConfigAzureCollector',      // g- Network Interface IP Configuration
+	'AzureLoadBalancerFrontendIPConfigAzureCollector',  // h- Load Balancer Frontend IP Configuration
+	'AzureVNetGatewayAzureCollector',                   // Virtual Network gateways
 ];
 
 // Initialize collection plan
