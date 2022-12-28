a2ced1507cd1c66fc1ea27a6663476a03433a466
xtophe38 <christophe.naud@gmail.com>
2022-12-28 17:04:11 +0100

    Adapt collector to TeemIp optional presence

--- src/AzureSubnetAzureCollector.class.inc.php
+++ src/AzureSubnetAzureCollector.class.inc.php
@@ -3,7 +3,9 @@ require_once(APPROOT.'collectors/msbase/src/MSJsonCollector.class.inc.php');
 
 class AzureSubnetAzureCollector extends MSJsonCollector
 {
-	// Required parameters to build URL
+    private $sIPv4Pattern = "((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))";
+
+    // Required parameters to build URL
 	protected static $aURIParameters = [
 		1 => self::URI_PARAM_SUBSCRIPTION,
 		2 => self::URI_PARAM_RESOURCEGROUP,
@@ -15,13 +17,17 @@ class AzureSubnetAzureCollector extends MSJsonCollector
 	 */
 	public function AttributeIsOptional($sAttCode): bool
 	{
-		if ($sAttCode == 'services_list') {
-			return true;
-		} elseif (($sAttCode == 'ip') && ($this->oMSCollectionPlan->bTeemIpIsInstalled)) {
-			return true;
-		} elseif (($sAttCode == 'ip_id') && (!$this->oMSCollectionPlan->bTeemIpIsInstalled)) {
-			return true;
-		}
+		if ($sAttCode == 'services_list') return true;
+
+        if ($this->oMSCollectionPlan->IsTeemIpInstalled()) {
+            if ($sAttCode == 'ip') return true;
+            if ($sAttCode=='mask') return true;
+            if ($sAttCode=='ipsubnet_id') return false;
+        } else {
+            if ($sAttCode == 'ip') return false;
+            if ($sAttCode=='mask') return false;
+            if ($sAttCode=='ipsubnet_id') return true;
+        }
 
 		return parent::AttributeIsOptional($sAttCode);
 	}
@@ -55,10 +61,20 @@ class AzureSubnetAzureCollector extends MSJsonCollector
 	/**
 	 * @inheritdoc
 	 */
-	public function Prepare(): bool
+    protected function InitProcessBeforeSynchro(): void
+    {
+        // Create subnet mapping table
+        if ($this->oMSCollectionPlan->IsTeemIpInstalled()) {
+            $this->oSubnetIPv4Mapping = new LookupTable('SELECT IPv4Subnet', array('org_id_friendlyname', 'ip'));
+            $this->oSubnetIPv6Mapping = new LookupTable('SELECT IPv6Subnet', array('org_id_friendlyname', 'ip'));
+     }
+
+    }
+
+    public function Prepare(): bool
 	{
-		// Create MappingTable
-		$this->oSubnetMaskMapping = new MappingTable('subnet_mask_mapping');
+        // Create mask mapping table
+		$this->oIPv4SubnetMaskMapping = new MappingTable('ipv4_subnet_mask_mapping');
 
 		return parent::Prepare();
 	}
@@ -126,6 +142,16 @@ class AzureSubnetAzureCollector extends MSJsonCollector
 		if (!$this->Lookup($aLineData, array('primary_key'), 'azurevnet_id', $iLineIndex, true, false)) {
 			throw new IgnoredRowException('Unknown code');
 		}
+        if ($this->oMSCollectionPlan->IsTeemIpInstalled()) {
+            if ($iLineIndex == 0) {
+                // Make sure bith lookup tables are correctly initialized
+                $this->oSubnetIPv4Mapping->Lookup($aLineData, array('org_id', 'ipsubnet_id'), 'ipsubnet_id', 0);
+                $this->oSubnetIPv6Mapping->Lookup($aLineData, array('org_id', 'ipsubnet_id'), 'ipsubnet_id', 0);
+            } elseif (!$this->oSubnetIPv4Mapping->Lookup($aLineData, array('org_id', 'ipsubnet_id'), 'ipsubnet_id', $iLineIndex) &&
+                      !$this->oSubnetIPv6Mapping->Lookup($aLineData, array('org_id', 'ipsubnet_id'), 'ipsubnet_id', $iLineIndex)) {
+                throw new IgnoredRowException('Unknown Subnet');
+            }
+        }
 	}
 
 	/**
@@ -138,23 +164,29 @@ class AzureSubnetAzureCollector extends MSJsonCollector
 			// Then process specific data
 			$iJsonIdx = $this->iIdx - 1; // Increment is done at the end of parent::Fetch()
 			$aData['provisioning_status'] = strtolower($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['provisioningState']);
-			$aData['mask'] = $this->oSubnetMaskMapping->MapValue(trim(strstr($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['addressPrefix'], '/'), '/'), '');
 			if (array_key_exists('delegations', $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties'])) {
 				$aDelegations = $this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['delegations'];
 
 			}
 			$sIP = strstr($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['addressPrefix'], '/', true);
-			if ($this->oMSCollectionPlan->bTeemIpIsInstalled) {
-				$aData['ip_id'] = $sIP;
+			if ($this->oMSCollectionPlan->IsTeemIpInstalled()) {
+				$aData['ipsubnet_id'] = $sIP;
 				if (array_key_exists('ip', $aData)) {
 					unset($aData['ip']);
 				}
 			} else {
 				$aData['ip'] = $sIP;
-				if (array_key_exists('ip_id', $aData)) {
-					unset($aData['ip_id']);
+				if (array_key_exists('ipsubnet_id', $aData)) {
+					unset($aData['ipsubnet_id']);
 				}
 			}
+            $sMask = strstr($this->aJson[$this->aJsonKey[$iJsonIdx]]['properties']['addressPrefix'], '/');
+            if (preg_match($this->sIPv4Pattern, $sIP)) {
+                // Map /n mask format into x.y.z.t in case of IPv4 subnet
+                $aData['mask'] = $this->oIPv4SubnetMaskMapping->MapValue(trim($sMask, '/'), '');
+            } else {
+                $aData['mask'] = $sMask;
+            }
 		}
 
 		return $aData;
